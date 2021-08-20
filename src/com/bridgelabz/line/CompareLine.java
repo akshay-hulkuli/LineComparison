c8bfab623a405bd914d039d1d7a39c7d27fee3a4
Akshaya HP <akshayhulkuli@gmail.com>
2021-08-20 15:24:56 +0530

    MODIFIED based on UC2

--- src/com/bridgelabz/line/CompareLine.java
+++ src/com/bridgelabz/line/CompareLine.java
@@ -3,14 +3,24 @@ package com.bridgelabz.line;
 public class CompareLine {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparison Computaion program");
-		int x1=0,y1=0,x2=0,y2=0;
-		x1 = (int) Math.floor(Math.random()*100);
-		y1 = (int) Math.floor(Math.random()*100);
-		x2 = (int) Math.floor(Math.random()*100);
-		y2 = (int) Math.floor(Math.random()*100);
-		double len =  Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
-		System.out.println("the points are (x1,y1):("+x1+","+y1+")  (x2,y2):("+x2+","+y2+")");
-		System.out.println("the length is : "+ len);
+		int p1x1=0,p1y1=0,p1x2=0,p1y2=0,p2x1=0,p2x2=0,p2y1=0,p2y2=0;
+		p1x1 = (int) Math.floor(Math.random()*100);
+		p1y1 = (int) Math.floor(Math.random()*100);
+		p1x2 = (int) Math.floor(Math.random()*100);
+		p1y2 = (int) Math.floor(Math.random()*100);
+		p2x1 = (int) Math.floor(Math.random()*100);
+		p2y1 = (int) Math.floor(Math.random()*100);
+		p2x2 = (int) Math.floor(Math.random()*100);
+		p2y2 = (int) Math.floor(Math.random()*100);
+		Double len1 =  Math.sqrt(Math.pow(p1x2-p1x1,2)+Math.pow(p1y2-p1y1,2));
+		Double len2 =  Math.sqrt(Math.pow(p2x2-p2x1,2)+Math.pow(p2y2-p2y1,2));
+		System.out.println("the end points of 1st line are (x1,y1):("+p1x1+","+p1y1+")  (x2,y2):("+p1x2+","+p1y2+")");
+		System.out.println("the length is : "+ len1);
+		System.out.println("the end points of 2nd line are (x1,y1):("+p2x1+","+p2y1+")  (x2,y2):("+p2x2+","+p2y2+")");
+		System.out.println("the length is : "+ len2);
+		System.out.println("Are they equal in length : "+len1.equals(len2));
+		
+		
 	}
 
 }
