091deebd2a48601e9de1e2ea06bcbdf47f30146a
Akshaya HP <akshayhulkuli@gmail.com>
2021-08-20 14:52:45 +0530

    Initial upload

---
+++ src/com/bridgelabz/line/CompareLine.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.line;
+
+public class CompareLine {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparison Computaion program");
+	}
+
+}
