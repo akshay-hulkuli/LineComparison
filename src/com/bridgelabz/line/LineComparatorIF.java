a98639d15659453a7c3dd7799cf73bb82b646520
Akshaya HP <60184210+akshay-hulkuli@users.noreply.github.com>
2021-08-30 21:52:38 +0530

    ADDED: interface defining line comparator

--- src/com/bridgelabz/line/LineComparatorIF.java
+++ src/com/bridgelabz/line/LineComparatorIF.java
@@ -3,4 +3,4 @@ package com.bridgelabz.line;
 public interface LineComparatorIF {
 	public void compareUsingEquals(Line line1 , Line line2);
 	public void compareUsingCompareTo(Line line1 , Line line2);
-}
+} 
